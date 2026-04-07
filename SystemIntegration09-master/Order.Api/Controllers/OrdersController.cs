f8fdc126efbbce9855359b8dd83c47c808bbc524
Maja <onlineshit61@gmail.com>
2026-04-07 10:39:07 +0200

    Lektion 9 - Opgave 2 udført

--- SystemIntegration09-master/Order.Api/Controllers/OrdersController.cs
+++ SystemIntegration09-master/Order.Api/Controllers/OrdersController.cs
@@ -1,14 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
 using Orders.Api.data;
 using Orders.Api.Models;
 using Orders.Api.Services;
 
-namespace Orders.Api.Controllers
-{
+namespace Orders.Api.Controllers {
     [Route("api/[controller]")]
     [ApiController]
-    public class OrdersController : ControllerBase
-    {
+    public class OrdersController : ControllerBase {
         private readonly OrdersContext _context;
         private readonly IShippingMessageSender _sender;
         private readonly ILogger<OrdersController> _logger;
@@ -17,8 +16,7 @@ namespace Orders.Api.Controllers
             OrdersContext context,
             IShippingMessageSender sender,
             ILogger<OrdersController> logger
-            )
-        {
+        ) {
             _context = context;
             _sender = sender;
             _logger = logger;
@@ -26,30 +24,21 @@ namespace Orders.Api.Controllers
 
         // POST: api/Orders
         [HttpPost]
-        public async Task<ActionResult<Order>> PostOrder(Order order)
-        {
+        public async Task<ActionResult<Order>> PostOrder(Order order) {
             order.Id = Guid.NewGuid();
             _context.Orders.Add(order);
+            string theOrder = JsonSerializer.Serialize(order);
+            OutboxMessage outboxMessage = new OutboxMessage { Type = "OrderCreated", Payload = theOrder };
+            _context.OutboxMessages.Add(outboxMessage);
             await _context.SaveChangesAsync();
-            try
-            {
-                await _sender.SendMessageAsync(order);
-            }
-            catch (Exception exception)
-            {
-                _logger.LogError(exception, "Failed to send message to RabbitMQ");
-
-            }
             return CreatedAtAction("PostOrder", new { id = order.Id }, order);
         }
 
         // DELETE: api/Orders/5
         [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteOrder(Guid id)
-        {
+        public async Task<IActionResult> DeleteOrder(Guid id) {
             var order = await _context.Orders.FindAsync(id);
-            if (order == null)
-            {
+            if (order == null) {
                 return NotFound();
             }
 
@@ -59,8 +48,7 @@ namespace Orders.Api.Controllers
             return NoContent();
         }
 
-        private bool OrderExists(Guid id)
-        {
+        private bool OrderExists(Guid id) {
             return _context.Orders.Any(e => e.Id == id);
         }
     }
